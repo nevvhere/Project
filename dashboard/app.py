cad539dd72c6ac18dfc19a948578d2b6300bdbf3
margoblina <margozagitova@icloud.com>
2025-05-14 19:00:34 +0300

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,7 +7,7 @@ from collections import Counter
 
 # Загрузка CSS-стилей
 def load_css():
-    with open("assets/style.css") as f:
+    with open("dashboard/assets/style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Загрузка данных
